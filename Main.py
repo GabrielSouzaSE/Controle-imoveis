8ab888901e6bc31988b0f3acdf3eb73792dc1b44
Luís Felipe Monteiro <felipemonteirose@gmail.com>
2020-12-03 21:03:21 -0300

    .

--- Main.py
+++ Main.py
@@ -1,22 +1,14 @@
 from Classes import Proprietario, Imovel, Inquilino, Aluguel
-from Funcao import buscarDisciplina, buscarProfessor, voltarMenu, criarDataFrame, \
-    salvarDataframe, getDataFramefromExcel, validar_professor, validar_aluno, validar_disciplina, validar_nota
+from Funcao import cadastrarProprietario, cadastrarImovel, cadastrarInquilino, registrarAluguel, finalizarAluguel
 
-try:
-    open('Dados.xlsx', 'r')
+# try:
+#     open('Dados.xlsx', 'r')
+#
+# except IOError:
+#     print('Criando novos arquivos...')
+#     criarDataFrame()
 
-except IOError:
-    print('Criando novos arquivos...')
-    criarDataFrame()
-
-listaProp = []
-listaImovel = []
-listaInquilino = []
-lista = []
-notas = []
-alunos = []
-
-getDataFramefromExcel(listaProf, listaAluno, listaDisc, listaNota)
+#getDataFramefromExcel(listaProf, listaAluno, listaDisc, listaNota)
 
 while True:
     print('\nMenu de Imóvel\n\n'
@@ -35,24 +27,24 @@ while True:
     escolha = input('Escolha uma das opções acima: ')
 
     if escolha == '0':
-        salvarDataframe()
+        #salvarDataframe()
         print('\nFim do programa.')
         break
 
     elif escolha == '1':
-        validar_professor(listaProf)
+        cadastrarProprietario()
 
     elif escolha == '2':
-        validar_aluno(listaAluno)
+        cadastrarImovel()
 
     elif escolha == '3':
-        validar_disciplina(listaDisc)
+        cadastrarInquilino()
 
     elif escolha == '4':
-        validar_nota(listaNota)
+        registrarAluguel()
 
     elif escolha == '5':
-        pass
+        finalizarAluguel()
 
     else:
         print('\nOpção invalida! Digite novamente.')
